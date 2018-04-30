870795b4e3b6f6117b8134041acbaa4ee389f150
Sam Gehman <sgehman@cs.uw.edu>
2018-04-29 18:52:02 -0700

    Added some files and a podfile

---
+++ MedicOnePedsCards/BigButtonCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BigButtonCollectionViewCell.swift
+//  MedicOnePedsCards
+//
+//  Created by Sam Gehman on 4/27/18.
+//  Copyright © 2018 Sam Gehman. All rights reserved.
+//
+
+import UIKit
+
+class BigButtonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
