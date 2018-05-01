ce0db4c89996b3ae8990777535642f2879400648
Sam Gehman <sgehman@cs.uw.edu>
2018-04-30 20:55:04 -0700

    Implemented EntryTableViewCell

---
+++ MedicOnePedsCards/EntryTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  EntryTableViewCell.swift
+//  MedicOnePedsCards
+//
+//  Created by Sam Gehman on 4/30/18.
+//  Copyright © 2018 Sam Gehman. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var treatmentLabel: UILabel!
+    @IBOutlet weak var doseLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var volumeLabel: UILabel!
+    @IBOutlet weak var administerButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
